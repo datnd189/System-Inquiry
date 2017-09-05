8a8d1d8195237689b97eadcdc14b164966e1a1e3
nddat <nddat@gmail.com>
2017-09-05 11:11:53 +0900

    code git init

---
+++ wwwroot/db_config.php
@@ -0,0 +1,15 @@
+<?php
+// db_config.php
+
+// DBÚ‘±İ’è‚ğreturn‚·‚é
+function db_config() {
+  //
+  return array(
+      'user' => 'datnd',
+      'database' => 'datnd',
+      'pass' => 'datnd',
+      'host' => 'localhost',
+      'charset' => 'utf8mb4',
+    );
+}
+
